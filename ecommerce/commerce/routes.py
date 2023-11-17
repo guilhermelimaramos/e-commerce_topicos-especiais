8dfbe2a077a0bbf25976947856a7b4397ffa07a5
Guilherme Lima <glimaramos013@gmail.com>
2023-11-17 12:40:24 -0300

    sign up route

--- ecommerce/commerce/routes.py
+++ ecommerce/commerce/routes.py
@@ -1,6 +1,7 @@
 from commerce import app
 from flask import render_template
 from commerce.models import Product
+from forms import SignUpForm
 
 @app.route('/')
 def page_home():
@@ -10,3 +11,8 @@ def page_home():
 def page_products():
   product = Product.query.all()
   return render_template('products.html', product=product)
+
+@app.route('/signup')
+def page_signup():
+  form = SignUpForm()
+  return render_template('signup.html', form=form)
